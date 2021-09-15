acae23a47aaa41e43d35515be14619314bb7fcdb
Fab9411 <55483523+Fab9411@users.noreply.github.com>
2021-09-15 19:15:32 +0200

    implemented payoff, probably not in optimal way

--- pages.py
+++ pages.py
@@ -11,30 +11,6 @@ class Anagraphics (Page):
     def is_displayed(self):
         return self.round_number == 1
 
-class ResultsWaitPage(WaitPage):
-    pass
-
-class Questions(Page):
-    form_model = 'player'
-    form_fields = ['choices']
-
-    
-
-    def vars_for_template(self):
-        # selecting the row of the news_df that corresponds to the current round
-        row = Constants.news_df.iloc[self.round_number - 1, :]
-        self.subsession.news_title = row['title']
-        self.subsession.news_text = row['text']
-        self.subsession.news_type = row['type']
-        return{
-        'news_title': row['title'],
-        'news_text': row['text'],
-        'source': row['type'],
-        'round_number':self.round_number,
-        }
-    
-
-
 class Check_Anagraphics(Page):
 
     def is_displayed(self):
@@ -45,8 +21,49 @@ class Check_Anagraphics(Page):
             'your_age': self.player.age
         }
 
+class Questions(Page):
+    form_model = 'player'
+    form_fields = ['choices']
+
+
+    def vars_for_template(self):
+        # selecting the row of the news_df that corresponds to the current round
+        row = Constants.news_df.iloc[self.round_number - 1, :]
+        self.subsession.news_title = row['title']
+        self.subsession.news_text = row['text']
+        self.subsession.news_type = row['type']
+        
+        return{
+        'news_title': row['title'],
+        'news_text': row['text'],
+        'source': row['type'],
+        'round_number':self.round_number,
+        }
+
 class Results(Page):
+    """
+    In this page we show the answer of the player, the correct answer, the relative payoff and the
+    total payoff.
+    IMPORTANT: to be shows only when 'debunking' mechanism is wanted
+    To be done:
+        - include debunking treatment and make this page dependent on it
+    """
+    
+    
+    def vars_for_template(self):
+        if self.subsession.news_type == self.player.choices:
+            self.player.payoff = 1
+        else:
+            self.player.payoff = 0
+
+        return{
+        'news_type': self.subsession.news_type,
+        'choice': self.player.choices
+        }       
+    
+
+class Final_Results(Page):
     def is_displayed(self):
         return self.round_number == 10
     
-page_sequence = [Anagraphics, Check_Anagraphics, Questions, ResultsWaitPage, Results]
+page_sequence = [Anagraphics, Check_Anagraphics, Questions, Results, Final_Results]
